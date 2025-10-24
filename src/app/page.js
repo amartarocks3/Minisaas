326d748552bb1867d0fce1a2e01b3e4eb7e71b92
Amarta Sarkar <amarta.sarkar@neorox.com>
2025-10-24 11:12:29 +0530

    Added: Final touch

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+"use client";
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <h1>hello</h1>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/signup'); // redirect to signup on page load
+  }, [router]);
+
+  return null; // or loading indicator
 }
